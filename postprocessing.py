75c40a1350f6f052b64ecdb0dfcb92d0971021a2
lukasmeggle <146966351+lukasmeggle@users.noreply.github.com>
2023-11-05 15:39:06 +0100

    postprocessing.py: added import packages

--- postprocessing.py
+++ postprocessing.py
@@ -1,3 +1,9 @@
+import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.manifold import Isomap
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+import matplotlib.pyplot as plt
+
 def plot_data(data, labels):
 # Method to plot data as PCA and ISOMAP representation with the first two components.
 # input has to be: np.array for data without labels, and labels as label encoded classes (0,1,2,..)
